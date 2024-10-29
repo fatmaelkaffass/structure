c02ae095f1c5b5ec0c636471143993443d8549e4
fatmaelkaffass <118898436+fatmaelkaffass@users.noreply.github.com>
2024-10-29 21:59:21 +0300

    Update customercontroller.java

--- src/main/java/struck1/customer/customercontroller.java
+++ src/main/java/struck1/customer/customercontroller.java
@@ -1,6 +1,6 @@
 package struck1.customer;
-@RestController
-@RequestMapping 
+@RestController //Annotates a class to indicate that it serves as a controller in Spring MVC to handle HTTP requests.
+@RequestMapping // handle specific HTTP requests.
   
 public class customercontroller {
 }
